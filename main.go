afb6ed37d73a8b2c5f5e40531820a3d4118a9d80
AmanKhys <amankhys2112@gmail.com>
2024-04-25 07:00:54 +0530

    make a gorm db connection make a TodoItemModel struct which can be converted to a gorm db table; make a handler function CreateItem which creates a todo list in the table created by by using DropTableIfExits, AutoMigrate methods on the db in debug mode.

--- main.go
+++ main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"encoding/json"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	log "github.com/sirupsen/logrus"
 	"io"
 	"net/http"
+	"strconv"
 )
+
+const (
+	dbDriver = "mysql"
+	dbUser   = "root"
+	dbPass   = "pass"
+	dbName   = "todo_app"
+)
+
+var db, _ = gorm.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName+"?charset=utf8&parseTime=True&loc=Local")
+
+type TodoItemModel struct {
+	Id          int `gorm:"primary key"`
+	Description string
+	Completed   bool
+}
 
 func Healthz(w http.ResponseWriter, r *http.Request) {
 	log.Info("API health is okay")
@@ -18,9 +38,25 @@ func init() {
 	log.SetReportCaller(true)
 }
 
+func CreateItem(w http.ResponseWriter, r *http.Request) {
+	description := r.FormValue("description")
+	log.WithFields(log.Fields{"description": description}).Info("Add new Todo Item. Saving to the database. ")
+	todo := &TodoItemModel{Description: description, Completed: false}
+	db.Create(&todo)
+	result := db.Last(&todo)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(&result.Value)
+}
+
 func main() {
+	defer db.Close()
+
+	db.Debug().DropTableIfExists(&TodoItemModel{})
+	db.Debug().AutoMigrate(&TodoItemModel{})
+
 	log.Info("Starting todo list API")
 	router := mux.NewRouter()
 	router.HandleFunc("/Healthz", Healthz).Methods("GET")
+	router.HandleFunc("/todo", CreateItem).Methods("POST")
 	http.ListenAndServe(":8000", router)
 }
